ffd3004d90d52297c83d1888d5254aad3e03df0c
Mark Lobodzinski <mark@lunarg.com>
2017-12-04 15:14:26 -0700

    layers: Add load_op_load and Layout_Undefined trkr
    See Github #2248.
    
    Change-Id: I408361b3309878982c816d5aabd897fca12f867b

--- layers/state_tracker_objects.h
+++ layers/state_tracker_objects.h
@@ -1,3 +1,4 @@
 #include "state_trackers\vllf_mem_objects.h"
 #include "state_trackers\vllf_zero_counts.h"
 #include "state_trackers\vllf_stage_all_warn.h"
+#include "state_trackers\vllf_load_op_load_undefined.h"
